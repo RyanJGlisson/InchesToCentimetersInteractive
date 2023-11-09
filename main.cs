8e83aefdcb06b1d16fb024c49285df0bc81bddbd
RyanJGlisson <143763150+RyanJGlisson@users.noreply.github.com>
2023-11-09 21:17:50 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,25 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+     //Establish
+        const double centInInch = 2.54;
+        double inches;
+        double centimeters;
+
+      //Get input from user
+        Console.Write("Enter length in inches: ");
+        string input = Console.ReadLine();
+
+      if (double.TryParse(input, out inches)) {
+          // Calculate centimeters
+          centimeters = inches * centInInch;
+
+          // Display the result
+          Console.WriteLine(inches + " inches is " + centimeters + " centimeters");
+      } else {
+          // Display an error message for invalid input
+          Console.WriteLine("Invalid input. Please enter a valid number for inches.");
+        }
+    }
+  }
